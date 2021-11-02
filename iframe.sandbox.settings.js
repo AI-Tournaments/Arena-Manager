d82cbe1a2c963f31c245a51b1632795ad5cd2ef8
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2021-11-02 08:38:51 +0100

    Fixed href.

--- iframe.sandbox.settings.js
+++ iframe.sandbox.settings.js
@@ -94,7 +94,7 @@ function a(){
 				let iframedMessage = document.createElement('iframe');
 				iframedMessage.sandbox = '';
 				iframedMessage.classList.add('message');
-				iframedMessage.srcdoc = '<!DOCTYPE html><html><head><link rel="stylesheet" href="../defaults.css"></head><body>'+comment.message+'</body></html>';
+				iframedMessage.srcdoc = '<!DOCTYPE html><html><head><link rel="stylesheet" href="https://ai-tournaments.github.io/AI-Tournaments/defaults.css"></head><body>'+comment.message+'</body></html>';
 				if(comment.height !== undefined){
 					iframedMessage.height = comment.height;
 				}
